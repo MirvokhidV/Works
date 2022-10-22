f65c8473a07b4f751646c0674f5c4bc0b54161cb
Mirvokhid Vosiljonov <mirvohidvosiljonov@gmail.com>
2022-10-23 02:27:17 +0500

    Simple patter
    from the quiz one

---
+++ Quiz 1/quiz1.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	string name;
+	cout << "Input your name: "; cin >> name;
+	int num, digit,j=0,ans,ans1,num1,m;
+	cout << "Enter number: "; cin >> num;
+	if (num<0)
+	{
+		num = abs(num);
+	}
+	digit = num;
+	num1 = num;
+	while (digit>=0)
+	{
+		
+		digit /= 10;
+		j++;
+		if (digit==0)
+		{
+			break;
+		}
+	}
+	if (j==10)
+	{
+		cout << "Your number is too large, the program might work wrongly" << endl;
+	}
+	cout << num << " has " << j << " digits" << endl;
+	cout << " " << endl;
+	for (int k = pow(10,j); k >=0 ; k/=10)
+	{
+		ans1 = num1 % k;
+		if (ans1 == 0)
+		{
+			break;
+		}
+		cout << ans1 << endl;
+		
+	}
+	m = j-1;
+	while (m>=0)
+	{
+		m--;
+		cout << "-";
+	}
+	cout << " " << endl;
+	for (int i = pow(10, j-1); i >= 0; i /= 10)
+	{
+		ans = num / i;
+		cout << ans << endl;
+		if (ans==0)
+		{
+			break;
+		}
+	}
+
+
+
+	system("Pause");
+	return 0;
+}
